8082a7f482b06dc6736f530789e88d0d4dc2bd5c
danielani24-meet <danielanir@gmail.com>
2023-07-17 18:30:29 +0300

    minimum requirements + bonus try

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function changeBackgroundColor(color) {
-	document.body.style.backgroundColor = "grey";
+function changeBackgroundColor() {
+	
+	getElementByTagName(body);
+	if (document.body.style.backgroundColor == "lightsteelblue"){
+		document.body.style.backgroundColor = "grey";
+		console.log("changed to blue");
+	}
+	else  {
+		if(document.body.style.backgroundColor == "grey"){
+			document.body.style.backgroundColor = "lightsteelblue";
+			console.log("changed to grey");
+		}
+
+	}
+
+
 }
+document.body.style.backgroundColor = "lightsteelblue";
